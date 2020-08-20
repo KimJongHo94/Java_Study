50f928df74d8b2a4882be776b2eec6f3d4842662
KimJongHo94 <joh963@naver.com>
2020-08-20 21:11:57 +0900

    자바 공부

---
+++ Java_Study/src/ch01/HelloError.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class HelloError {
+
+	public static void main(String[] args) {
+		
+		// System.out.println("첫 Java 프로그램입니다.")
+		// System.out.println("화면에 출력하고 있습니다.")
+		// Syntax error, insert ";" to complete BlockStatements
+		// 컴파일 오류
+		// ; 생략해서 나오는 오류
+	}
+
+}
