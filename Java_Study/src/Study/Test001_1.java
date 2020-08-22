49640bd456eff864f97972590b97d26800003605
KimJongHo94 <joh963@naver.com>
2020-08-22 21:45:42 +0900

    자바 공부

---
+++ Java_Study/src/Study/Test001_1.java
@@ -0,0 +1,48 @@
+package Study;
+
+/*
+	●●● 자바의 개요 및 특징 ●●●
+*/
+
+public class Test001_1 {
+
+	//main 메소드
+	public static void main(String[] args) {
+		
+		// 메소드 안에 있는 문장이 순차적으로 진행된다.
+
+		// 자바에서의 ;(세미콜론) => 종결자
+
+		System.out.println("아무개");
+		System.out.print("010-0000-1234");
+		System.out.println("서울특별시 강남구 OOO OOO");
+
+		// System.out.println(아무개);  ==> 에러 발생(컴파일 에러)
+		// ==> 실행과정에서 발생하는 에러 = 런타임 에러
+
+		System.out.println(100);
+
+		System.out.println(3.14159);
+
+		System.out.println(10 + 2);
+
+		// System.out.println(빵);   ==> 컴파일 에러
+
+		// System.out.println(A);    ==> 컴파일 에러
+	}
+}
+
+
+
+/*
+   실행 결과
+
+아무개
+010-0000-1234서울특별시 강남구 OOO OOO
+100
+3.14159
+12
+계속하려면 아무 키나 누르십시오 . . .
+
+*/
+	
