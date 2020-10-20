dd6e3b68e96f55ded428ef9b9e57268b6e872d45
Nick65 <55225790+KimJongHo94@users.noreply.github.com>
2020-10-20 23:42:00 +0900

    자바 공부

---
+++ Java_Study/src/ch02/SumAve1.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class SumAve1 {
+
+	public static void main(String[] args) {
+
+		int x;	// x는 int형 변수
+		int y;
+		
+		x = 63;
+		y = 18;
+		
+		System.out.println("x값은 " + x + "입니다.");
+		System.out.println("y값은 " + y + "입니다.");
+
+	}
+
+}
---
+++ Java_Study/src/ch02/SumDiff1.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class SumDiff1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(82 + 17);
+		System.out.println(82 - 17);
+
+	}
+
+}
